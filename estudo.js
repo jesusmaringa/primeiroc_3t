da83bf6fc2ddc902a1def35d9e08f605c97e7c06
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2022-10-20 12:46:15 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -28,3 +28,37 @@ function cubo(){
         document.write("O cubo de "+i+" é "+i+" x "+i+" x "+i+" = "+(i*i*i)+"<br>");
     }
 }
+function moeda(atual){
+    return atual.toLocaleString('pt-br',{style: 'currency', currency: 'BRL'});
+}
+function mat(){  
+    let v = document.getElementById("valor").value;
+    let j = document.getElementById("juros").value;
+    let t = document.getElementById("messes").value;
+  
+    if(!Number(v)){
+        alert("O Capitual deve ser numérico.");
+        document.getElementById("juros").value = "";
+        document.getElementById("juros").focus();
+        return
+    }
+    if(!Number(j)){
+        alert("Juros deve ser número.");
+        document.getElementById("valor").value = "";
+        document.getElementById("valor").focus();
+        return
+    }
+    if(!Number(t)){
+        alert("Juros deve ser número.");
+        document.getElementById("meses").value = "";
+        document.getElementById("meses").focus();
+        return
+    }
+    let r = 0;
+    for(let i=1; i<=t; i++){
+        r = v * (1+(j/100));
+        v = r;
+        document.write("Mês " + i + " = " + moeda(r) + "<br>");
+    }
+    document.write("Resultado " + moeda(r));
+}
